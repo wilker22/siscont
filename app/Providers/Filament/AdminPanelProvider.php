46430ab821b4d8cff9bf51c1e27dfc1c7c8301ba
Wilker <wilker.silva@codevasf.gov.br>
2024-07-08 21:19:18 -0300

    Update AdminPanelProvider.php
    inclusão do spa()

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,6 +54,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])->spa();
     }
 }
